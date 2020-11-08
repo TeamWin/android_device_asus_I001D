b45c20dcd3a343b894df22293da017a6e82d02b3
Captain Throwback <captainthrowback@hotmail.com>
2020-11-08 12:58:07 -0500

    I001D: clean up tree and use twrp-common for decrypt
    - Update recovery.fstab and remove twrp.fstab
    - Add event-log-tags for logd
    - Update USB config and add UMS support
    - Delete custom installer (use standard @osm0sis version)
    - Delete other unnecessary files
    
    Change-Id: I370d837b4c9be4735dcd0ffbbd583787548cb73e

--- device.mk
+++ device.mk
@@ -1,11 +1,22 @@
-
 LOCAL_PATH := device/asus/I001D
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # define hardware platform
 PRODUCT_PLATFORM := msmnile
 TARGET_USES_HARDWARE_QCOM_BOOTCTRL := true
 
-#TEST
+# A/B updater updatable partitions list. Keep in sync with the partition list
+# with "_a" and "_b" variants in the device. Note that the vendor can add more
+# more partitions to this list for the bootloader and radio.
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vendor \
+    vbmeta \
+    dtbo
+
 # A/B support
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -14,7 +25,8 @@ PRODUCT_PACKAGES += \
     update_verifier
 
 PRODUCT_PACKAGES += \
-    bootctrl.msmnile
+    bootctrl.$(PRODUCT_PLATFORM) \
+    update_engine_sideload
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -25,15 +37,38 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Enable update engine sideloading by including the static version of the
 # boot_control HAL and its dependencies.
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.msmnile \
+    bootctrl.$(PRODUCT_PLATFORM) \
     libgptutils \
     libz \
     libcutils
+
+# qcom standard decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe \
+
+# tzdata
+PRODUCT_PACKAGES += \
+    tzdata_twrp \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service \
+
+# Blacklist
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.bootimage.build.date.utc \
+    ro.build.date.utc
+
+# Overrides
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=WW_I001D \
+    BUILD_PRODUCT=ZS660KL \
+    TARGET_DEVICE=ASUS_I001_1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
 
 # Include ASUS keys for zip signature verification
 PRODUCT_EXTRA_RECOVERY_KEYS += $(LOCAL_PATH)/security/asus
